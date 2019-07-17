1cf04d132cc55ea35d233bcc0713403a6842db40
ming <mkl0819@naver.com>
2019-07-17 12:28:12 +0900

    [MOD] Aboutus Page

--- src/components/Repository.vue
+++ src/components/Repository.vue
@@ -1,75 +1,76 @@
 <template>
 <div class="py-3">
-  <v-layout>
+  <v-layout style="display:block">
 
-    <v-dialog>
-      <!-- <v-flex xs-12 sm-8 md-6> -->
+    <!-- <v-dialog> -->
+    <!-- <v-flex xs-12 sm-8 md-6> -->
 
-      <!-- <v-dialog v-model="dialog"> -->
-      <template v-slot:activator="{ on }">
-        <v-hover>
+    <!-- <v-dialog v-model="dialog"> -->
+    <!-- <v-flex xs12 class="repo_item"> -->
+    <div class="repo_item">
+      <div class="oneline" style="width:80%; margin:auto">
+        <h2 class="font-weight-regular repo-txt" style="cursor:pointer" @click.stop="comlist = true">{{repos.path_with_namespace}}</h2>
+        <p class="subheading mb-1 grey--text text--darken-1 font-weight-light">{{repos.namespace.name}}</p>
+      </div>
+    </div>
+    <!-- </v-flex> -->
 
-          <v-flex xs4 v-on="on" slot-scope="{ hover }" style="cursor:pointer" class="repo_item">
-            <div class="oneline" style="width:100%;">
-              <h2 class="font-weight-regular repo-txt" >{{repos.path_with_namespace}}</h2>
-              <p class="subheading mb-1 grey--text text--darken-1 font-weight-light">{{repos.namespace.name}}</p>
-            </div>
-          </v-flex>
-        </v-hover>
-      </template>
+    <!-- <v-flex xs12 sm8 md6 center ma-auto> -->
 
-      <!-- <v-flex xs12 sm8 md6 center ma-auto> -->
-      <v-card xs-12>
-        <v-card-title primary-title hover>
-          <div v-model="commits.data">
-            <div class="caption">{{repos.namespace.name}}</div>
-            <h2 class="color-333 headline font-weight-light">{{repos.path_with_namespace}}</h2>
-
-            <v-list three-line>
-              <template v-for="(commit, index) in commits.data">
-                <v-divider></v-divider>
-
-                <v-list-tile @click="">
-                  <v-list-tile-content>
-                    <div class="caption">{{commit.author_name}} / {{commit.committed_date.substr(0, 10)}}</div>
-                    <v-list-tile-title>{{commit.title}}</v-list-tile-title>
-                    <v-list-tile-sub-title class="repo-txt">{{commit.message}}</v-list-tile-sub-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-              </template>
-            </v-list>
-          </div>
-        </v-card-title>
-      </v-card>
-      <!-- </v-flex> -->
-    </v-dialog>
-    <v-flex xs8>
-      <v-card
-              class="hidden-xs-only mx-auto text-xs-center"
-              color="#E4BD5B"
-              dark
-              sm-12
-      >
-          <v-sheet color="#fff">
-            <v-sparkline
-                    :value="modifyValue()"
-                    line-width = "1"
-                    smooth
-                    color="#ff0000"
-                    height="100"
-                    stroke-linecap="round"
-            >
-              <template v-slot:label="item">
-                {{ item.index+1 }}월
-              </template>
-            </v-sparkline>
-          </v-sheet>
+    <!-- <v-flex xs8> -->
+    <v-card width="80%" style="margin:auto">
+      <div class="hidden-xs-only mx-auto text-xs-center"  dark  sm-12>
+        <v-sheet color="#fff" >
+          <v-sparkline
+                :value="modifyValue()"
+                line-width="1"
+                smooth
+                color="#AA00FF"
+                stroke-linecap="round"
+                :labels = 'labels'
+                label-size="3"
+                padding-bottom="36">
+            <!-- <template v-slot:label="item">
+              {{ item.index+1 }}월
+            </template> -->
+          </v-sparkline>
+        </v-sheet>
         <!-- <v-divider></v-divider>
         <v-card-actions class="justify-center">
           <v-btn :href="`https://lab.ssafy.com/${repos.path_with_namespace}`" block flat>Go to Repository</v-btn>
         </v-card-actions> -->
+      </div>
+    </v-card>
+    <!-- </v-flex> -->
+
+    <v-dialog v-model="comlist">
+      <v-card xs-12 color="deep-purple lighten-4">
+        <v-card-title primary-title hover>
+          <div v-model="commits.data">
+            <div class="caption">{{repos.namespace.name}}</div>
+            <h2 class="color-333 headline font-weight-light">{{repos.path_with_namespace}}</h2>
+          </div>
+        </v-card-title>
+        <v-list three-line>
+          <template v-for="(commit, index) in commits.data">
+
+
+            <v-list-tile @click="">
+              <v-list-tile-content>
+                <div class="caption">{{commit.author_name}} / {{commit.committed_date.substr(0, 10)}}</div>
+                <v-list-tile-title>{{commit.title}}</v-list-tile-title>
+                <v-list-tile-sub-title class="repo-txt">{{commit.message}}</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-divider></v-divider>
+          </template>
+        </v-list>
+
+
       </v-card>
-    </v-flex>
+      <!-- </v-flex> -->
+    </v-dialog>
+
   </v-layout>
 
 </div>
@@ -90,8 +91,11 @@ export default {
   },
   data() {
     return {
+      comlist: false,
       stats: {},
-      commits: []
+      commits: [],
+      labels: [ '1월', '2월', '3월', '4월', '5월', '6월',
+                '7월', '8월', '9월', '10월', '11월', '12월', ]
     }
   },
   mounted() {
@@ -107,13 +111,17 @@ export default {
       // console.log('com : ' , this.commits);
       this.modifyValue();
     },
-    modifyValue(){
-      var value = [0,0,0,0,0,0,0,0,0,0,0,0];
+    modifyValue() {
+      var value = [0, 0, 0, 0, 0, 0, 0];
       // console.log(this.commits.data);
       // console.log(this.commits.data.length);
-      for(var i=0; i<this.commits.data.length; i++){
+      // for(var i=0; i<this.commits.data.length; i++){
+      //   let date = new Date(this.commits.data[i].committed_date);
+      //   value[date.getMonth()]+=1;
+      // }
+      for (var i in this.commits.data) {
         let date = new Date(this.commits.data[i].committed_date);
-        value[date.getMonth()]+=1;
+        value[date.getMonth()] += 1;
       }
       return value;
     }
@@ -123,8 +131,12 @@ export default {
 </script>
 
 <style>
-.repo_item > div {
-  display: inline-block;
+.repo_item {
+  width: 100%;
+}
+.repo_item>div {
+  /* display: inline-block; */
+  /* width: 100%; */
   vertical-align: middle;
   margin-right: 10px;
 }
@@ -134,5 +146,8 @@ export default {
   display: -webkit-box;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 1;
+}
+g{
+  dominant-baseline: auto!important;
 }
 </style>
